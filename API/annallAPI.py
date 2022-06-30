0a61fe44180ae7a9b0eaa453eea994a981fb3324
netflixfjolnir@gmail.com <netflixfjolnir@gmail.com>
2022-06-30 16:00:28 +0000

    Update annallAPI.py

--- API/annallAPI.py
+++ API/annallAPI.py
@@ -8,16 +8,16 @@ import sys
 from connectToServer import ServerConnection
 from exceptionHandler import InvalidUsage
 # import sys
-print("HELLO WORLD")
+print("Starting annallAPI Flask application server")
 app = Flask(__name__)
 # Connect to server
 TCP_PORT = 5011
 # if len(sys.argv) > 1:
 #     TCP_PORT = int(sys.argv[1])
 
-with open('/etc/config.json') as config_file:
-  config = json.load(config_file)
-app.config['SECRET_KEY'] = config.get('SECRET_KEY')
+# with open('/etc/config.json') as config_file:
+#   config = json.load(config_file)
+# app.config['SECRET_KEY'] = config.get('SECRET_KEY')
 
 server = ServerConnection(TCP_PORT)
 
