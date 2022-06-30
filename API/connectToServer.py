bc3a6ad9aeef6e17b02bc227e8fddecd30dd720c
netflixfjolnir@gmail.com <netflixfjolnir@gmail.com>
2022-06-30 11:06:12 +0000

    removed /db from gitignore and added *.db and *.sqliterc

--- API/connectToServer.py
+++ API/connectToServer.py
@@ -3,6 +3,9 @@ import sys
 import socket
 import json
 import time
+import asyncio
+import websockets
+
 # secret used to verify when connecting
 test_conf = {"writerlist": {}, "secret": "42"}
 test_conf["writerlist"][0] = {"ip": "127.0.0.1", "port": 15000}
