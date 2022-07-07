64f4bcb8d9a3fb255d736cb822f964cd7fa21953
Fjölnir <netflixfjolnir@gmail.com>
2022-07-07 11:00:20 +0000

    adding unittest

--- src/test_interfaces.py
+++ src/test_interfaces.py
@@ -25,12 +25,16 @@ print("[DB DIRECTORY] ",DB_PATH)
 connection = sqlite3.connect(DB_PATH)
 bcdb = BlockChainEngine(connection)
 
-the_block = ("prevHash", 1, 2, json.dumps({"hello":{"sailor":"the sailor"}}), 0, "writer signature", 1223123124134, "the hash")
+clean_block = ("prevHash", 1, 2, json.dumps({"hello":{"sailor":"the sailor"}}), 0, "writer signature", 1223123124134, "the hash")
 genesis_block = ("0", 0, 0,  "genesis block", 0, "0", 13413413434, "0")
+incorrect_type_block = ("prevHash", 1, 2, json.dumps({"hello":{"sailor":"the sailor"}}), 0, "writer signature", "1223123124134", "the hash")
+missing_field_block = ("prevHash", 1, 2, json.dumps({"hello":{"sailor":"the sailor"}}), 0, "writer signature", 1223123124134)
+non_json_block = ("prevHash", 1, 2, "string", 0, "writer signature", 1223123124134, "the hash")
 bcdb.insert_block(0, genesis_block)
-bcdb.insert_block(1, the_block)
-bcdb.insert_block(2, the_block)
-bcdb.insert_block(3, the_block)
+bcdb.insert_block(1, clean_block)
+bcdb.insert_block(2, incorrect_type_block)
+bcdb.insert_block(3, missing_field_block)
+bcdb.insert_block(3, non_json_block)
 msg = bcdb.read_blocks(0, 4)
 print(f"[MESSAGE READ BLOCKS 1-4] The message: {msg}")
 # time.sleep(100)
@@ -61,3 +65,15 @@ to_json = msg[0]["payload"]
 # pComm.join()
 # PEthread.join()
 # cthread.join()
+
+import unittest
+
+class TestBlockChainEngine(unittest.TestCase):
+    
+    def test_insert_block():
+        ...
+    
+    def test_read_blocks():
+        ...
+
+    def test_
