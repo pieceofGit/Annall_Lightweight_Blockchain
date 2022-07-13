a7727539347698d209bc45b4c16168c17729c6df
Fjölnir <netflixfjolnir@gmail.com>
2022-07-13 13:48:37 +0000

    script for running two writers and the API

---
+++ ss-run-writers-and-api.sh
@@ -0,0 +1,6 @@
+# (trap 'kill 0' SIGINT; python3 ./src/main.py -myID 1 -r 0 -conf config-local.json & python3 ./src/main.py -myID 2 -r 0 -conf config-local.json & gunicorn -w 1 annallAPI:app)
+# python3 ./src/main.py -myID 1 -r 0 -conf config-local.json & P1=$!
+# python3 ./src/main.py -myID 2 -r 0 -conf config-local.json & P2=$!
+# gunicorn -w 1 annallAPI:app & P3=$!
+# wait $P1 $P2 $P3
+python3 ./src/main.py -myID 1 -r 0 -conf config-local.json & python3 ./src/main.py -myID 2 -r 0 -conf config-local.json & gunicorn -w 1 --chdir ./API annallAPI:app & fg
