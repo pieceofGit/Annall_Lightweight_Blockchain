faee7e3a23e40e50e5e4566da3cff3a39242fcf9
Friðrik Ö. Gunnarsson <36244964+FridrikOG@users.noreply.github.com>
2022-07-18 16:25:43 +0000

    Added a bunch of necessary things for creating a wallet

--- ClientAPI/annallClientAPI.py
+++ ClientAPI/annallClientAPI.py
@@ -7,19 +7,14 @@ from flask import Flask, request, jsonify, Response
 import sys
 from connectToServer import ServerConnection
 from exceptionHandler import InvalidUsage
+from Crypto.Hash import SHA256
+
+
 # import sys
 print("Starting annallClientAPI Flask application server")
 app = Flask(__name__)
 # Connect to server
 TCP_PORT = 5001 # Connects to port of writer 1
-# if len(sys.argv) > 1:
-#     TCP_PORT = int(sys.ar gv[1])
-# ap = argparse.ArgumentParser()
-# help="input data file (default stdin)",
-# ap.add_argument("-conf", default="config-local.json", type=str, help="config file for writers")
-# a = ap.parse_args()
-# conf_file = a.conf
-
 
 with open(f'../src/config-local.json') as config_file:   # If in top directory for debug
   config = json.load(config_file)
@@ -28,14 +23,11 @@ with open(f'../src/config-local.json') as config_file:   # If in top directory f
 print(TCP_PORT)
 server = ServerConnection(IP_ADDR, TCP_PORT)
 
-
 @app.errorhandler(InvalidUsage)
 def handle_invalid_usage(error):
     response = jsonify(error.to_dict())
     response.status_code = error.status_code
     return response
-
-
 
 @app.route("/publishandsubscribe", methods=["GET"])
 def createSmartContracts():
@@ -43,24 +35,32 @@ def createSmartContracts():
     requestObject = getJson(request)
     typeToGet = requestObject['type']
     typeToGet = typeToGet.lower()
-    print("type to get ", typeToGet)
     resp_obj = server.send_msg(json.dumps({"request_type": "read_chain"}))
-    print("THe length of resp ", len(resp_obj))
     obj = json.loads(resp_obj)
     lis = []
     for res in obj:
         try:
-            print(res['payload']['headers']['type'], " versus ", typeToGet)
             if res['payload']['headers']['type'] == typeToGet:
                 lis.append(res)
-                
         except:
             print("Couldnt find a type")
     print("Return list for the given type ", lis)
+<<<<<<< Updated upstream
     # lis = json.loads(lis)
     print("Objects here ", obj)
     return jsonify(lis)
+=======
+    lis = jsonify(lis)
+    return Response(lis, mimetype="application/json")
+>>>>>>> Stashed changes
 
+
+@app.route("/walletTest", methods=["GET"])
+def testWallet():
+    # Asks for blockchain and gets it back
+    requestObject = getJson(request)
+    
+    return Response({}, mimetype="application/json")
 
 @app.route("/blocks", methods=["GET"])
 def get_blockchain():
---
+++ ClientAPI/wallet.py
@@ -0,0 +1,61 @@
+from Crypto.PublicKey import RSA
+from Crypto.Signature import pkcs1_15
+from Crypto.Hash import SHA256
+def signTransaction(privateKey, message = 'no message'):
+    ''' Takes in a private key and signs a transaction'''
+
+    # key = RSA.import_key(open('private.pem').read())
+    key = RSA.import_key(privateKey)
+    message = b'message'
+    hash = SHA256.new(message)
+    signature = pkcs1_15.new(key).sign(hash)
+    return hash, signature
+
+def getKeys():
+    ''' Returns a dictionary of a public and private key'''
+    keyDict = {}
+    key = RSA.generate(4096)
+    privateKey = key.export_key()
+    file_out = open("private.pem", "wb")
+    file_out.write(privateKey)
+    file_out.close()
+    keyDict['privateKey'] = privateKey
+    publicKey = key.publickey().export_key()
+    file_out = open("receiver.pem", "wb")
+    file_out.write(publicKey)
+    file_out.close()
+    keyDict['publicKey'] = publicKey
+    return keyDict 
+
+def signTransaction(privateKey, message = 'no message'):
+    ''' Takes in a private key and signs a transaction'''
+    print("Here I will sign a transaction")
+    # key = RSA.import_key(open('private.pem').read())
+    key = RSA.import_key(privateKey)
+    message = b'message'
+
+    hash = SHA256.new(message)
+    signature = pkcs1_15.new(key).sign(hash)
+    return hash, signature
+
+def verifySignature(pubKey, hash, signature):
+    ''' Verifies a signed message with the public key'''
+    pkcsObj = pkcs1_15.new(pubKey)
+    return pkcsObj.verify(hash, signature)
+
+# A dictionary of public and private keys
+userKeys = getKeys()
+message = 'My message to you'
+hash, signature = signTransaction(userKeys['privateKey'])
+pubKey = RSA.import_key(userKeys['publicKey'])
+# key = RSA.import_key(open('receiver.pem').read())
+print("The hash ", hash.hexdigest())
+try:
+    verifySignature(pubKey, hash, signature)
+    print("The signature is valid.")
+except (ValueError, TypeError):
+   print("The signature is not valid.")
+
+userKeys = getKeys()
+print("User key ", userKeys)
+
