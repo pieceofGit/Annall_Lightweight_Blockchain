bd728940804581a1e10f27117c5b963097d1efcb
Fjölnir <netflixfjolnir@gmail.com>
2022-07-29 11:21:59 +0000

    added cprofile run for node 1

--- ss1-local.sh
+++ ss1-local.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # programming-language file id transactionRounds 
-python3 -m cProfile -s tottime ./src/main.py -myID 1 -r 0 -conf config-local.json
+python3 ./src/main.py -myID 1 -r 0 -conf config-local.json
