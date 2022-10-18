a298729b767db6f47fd56c9e575b47bf1adb3604
Fjölnir <netflixfjolnir@gmail.com>
2022-10-18 14:15:30 +0000

    bash script update

--- ss2-remote-background.sh
+++ ss2-remote-background.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # programming-language file id transactionRounds 
-python3 ./src/main.py -myID 2 -r 0 -conf config-remote.json -writerApiPath http://176.58.116.107:70/  > writer2.out 2>&1
+python3 ./src/main.py -myID 2 -r 0 -conf config-remote.json -writerApiPath http://176.58.116.107:70/  > writer2.out 2>&1&
