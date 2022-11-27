2f94a1500f0978438fbc3a5e8f15e27ff0e9721b
Fjölnir <netflixfjolnir@gmail.com>
2022-11-27 15:37:40 +0000

    small update

--- ClientAPI/wsgiProd.py
+++ ClientAPI/wsgiProd.py
@@ -3,4 +3,4 @@ from . import init_prod_app
 app = init_prod_app()
 
 if __name__ == "__main__":
-    app.run(host=app.config["IP_ADDR"], port=app.config["PORT"])
+    app.run(host=app.config["HOST_IP"], port=app.config["HOST_PORT"])
