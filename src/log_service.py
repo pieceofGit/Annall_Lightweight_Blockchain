e1f9858baa8abc7aa72519c8c6f938af62369057
Fjölnir <netflixfjolnir@gmail.com>
2022-12-07 18:00:09 +0000

    Update log_service.py

--- src/log_service.py
+++ src/log_service.py
@@ -2,12 +2,6 @@
 import json
 import ssl
 from blockBroker import BlockBroker
-# parameters = pika.ConnectionParameters(
-#     host='b-1d97e372-ff08-4b61-85e1-5ad197032218.mq.us-west-2.amazonaws.com',
-#     port=5671,
-#     credentials=pika.PlainCredentials('hr-fintech-rabbitmq', 'wall_walker_212'))
-# context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-# connection = pika.BlockingConnection(parameters)
 client = BlockBroker()
 def log_data(ch, method, properties, data):
     data = json.loads(data)
