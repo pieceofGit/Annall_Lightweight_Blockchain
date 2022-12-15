d42d9e24bce43186765128af8c5b66e003cfbcaa
Fjölnir <netflixfjolnir@gmail.com>
2022-12-15 17:45:49 +0000

    consumer binds to declared queue

--- src/blockBroker.py
+++ src/blockBroker.py
@@ -12,24 +12,21 @@ class BlockBroker:
         self.setup_connection()
         
     def setup_connection(self):
-        try:
-             # SSL Context for TLS configuration of Amazon MQ for RabbitMQ
-            ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-            ssl_context.set_ciphers('ECDHE+AESGCM:!ECDSA')
-            # url parameters: amqps://{rabbitmq_user}:{rabbitmq_password}@{rabbitmq_broker_id}.mq.{region}.amazonaws.com:5671
-            url = f"amqps://hr-fintech-rabbitmq:wall_walker_212@b-1d97e372-ff08-4b61-85e1-5ad197032218.mq.us-west-2.amazonaws.com:5671"
-            parameters = pika.URLParameters(url)
-            parameters.ssl_options = pika.SSLOptions(context=ssl_context)
-            self.connection = pika.BlockingConnection(parameters)
-            self.channel = self.connection.channel()
-            # Declare the exchange, if it doesn't exist
-            self.channel.exchange_declare(exchange=self.exchange_name, exchange_type='direct', durable=True)
-            # Declare the queue, if it doesn't exist
-            # self.channel.queue_declare(queue=self.queue_name, durable=True)
-            # # Bind the queue to a specific exchange with a routing key
-            # self.channel.queue_bind(exchange=self.exchange_name, queue=self.queue_name, routing_key=self.routing_key)
-        except Exception as e:
-            print("Failed to setup connection", e)
+            # SSL Context for TLS configuration of Amazon MQ for RabbitMQ
+        ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+        ssl_context.set_ciphers('ECDHE+AESGCM:!ECDSA')
+        # url parameters: amqps://{rabbitmq_user}:{rabbitmq_password}@{rabbitmq_broker_id}.mq.{region}.amazonaws.com:5671
+        url = f"amqps://hr-fintech-rabbitmq:wall_walker_212@b-1d97e372-ff08-4b61-85e1-5ad197032218.mq.us-west-2.amazonaws.com:5671"
+        parameters = pika.URLParameters(url)
+        parameters.ssl_options = pika.SSLOptions(context=ssl_context)
+        self.connection = pika.BlockingConnection(parameters)
+        self.channel = self.connection.channel()
+        # Declare the exchange, if it doesn't exist
+        self.channel.exchange_declare(exchange=self.exchange_name, exchange_type='direct', durable=True)
+        # Declare the queue, if it doesn't exist
+        # self.channel.queue_declare(queue=self.queue_name, durable=True)
+        # # Bind the queue to a specific exchange with a routing key
+        # self.channel.queue_bind(exchange=self.exchange_name, queue=self.queue_name, routing_key=self.routing_key)
 
     def queue_connect(self, queue_name):
         # Declare the queue, if it doesn't exist
--- src/log_service.py
+++ src/log_service.py
@@ -2,12 +2,15 @@
 import json
 import ssl
 from blockBroker import BlockBroker
+queue = "log_queue" # Set the queue to connect to 
 client = BlockBroker()
 def log_data(ch, method, properties, data):
     data = json.loads(data)
     print(data)
+client.queue_connect(queue) # create and bind to queue
 client.channel.basic_consume(on_message_callback=log_data,
-                      queue=client.queue_name)
+                      queue=queue,
+                      auto_ack=True)
 
 client.channel.start_consuming()
 client.connection.close()
